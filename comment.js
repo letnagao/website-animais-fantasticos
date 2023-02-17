c85d3113c47823c34b58a16649e96f21cb153844
letnagao <leticia-nagao@hotmail.com>
2023-02-17 19:45:14 -0300

    Added strings constructures

---
+++ comment.js
@@ -0,0 +1,13 @@
+const comida = 'Pizza';
+const frase = 'A melhor comida';
+
+comida.length; // 5
+frase.length; // 15
+
+comida[0] // P
+frase[0] // A
+frase[frase.length - 1] // a
+
+const comida = 'Pizza';
+const liquido = new String('Água');
+const ano = new String(2018);
